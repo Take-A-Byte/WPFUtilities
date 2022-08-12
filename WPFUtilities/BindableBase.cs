c99fa12226960c3d6c13670678afc53be65baa8b
Shantanu.Methikar <smethikar18@gmail.com>
2022-08-12 19:16:51 +0300

    Introduce Bindable base to enable property changed notifications

---
+++ WPFUtilities/BindableBase.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WPFUtilities
+{
+    public class BindableBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public bool OnPropertyChanged(object oldValue, object newValue, [CallerMemberName]string? propertyName = null)
+        {
+            if (oldValue.Equals(newValue))
+            {
+                return false;
+            }
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            return true;
+        }
+    }
+}
